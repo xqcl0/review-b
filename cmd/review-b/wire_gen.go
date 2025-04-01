6e6f4a841c28b070fbafcca09e25b371623634cf
xqcl0 <2694956143@qq.com>
2025-04-01 16:42:08 +0800

    b端reply rpc

--- cmd/review-b/wire_gen.go
+++ cmd/review-b/wire_gen.go
@@ -24,7 +24,8 @@ import (
 
 // wireApp init kratos application.
 func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger) (*kratos.App, func(), error) {
-	dataData, cleanup, err := data.NewData(confData, logger)
+	reviewClient := data.NewReviewServiceClient()
+	dataData, cleanup, err := data.NewData(confData, logger, reviewClient)
 	if err != nil {
 		return nil, nil, err
 	}
